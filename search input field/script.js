f6b001def089943afe122e3059c94c796b0bd15a
Bandhavi1234 <138019371+Bandhavi1234@users.noreply.github.com>
2024-07-08 20:07:40 +0530

    Add files via upload

---
+++ search input field/script.js
@@ -0,0 +1,16 @@
+// Optional: Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+function selectCategory(category) {
+    document.getElementById('dropdownButton').textContent = category;
+}
